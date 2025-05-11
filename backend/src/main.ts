5c9f080f2a18661a7bbd72aee97000a885e0db98
Kailas Walldoddi <kailashwall@gmail.com>
2025-05-11 19:19:08 +0400

    docker set up

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { ApiKeyGuard } from './auth/api-key.guard';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   app.useGlobalGuards(new ApiKeyGuard(config));
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   // Swagger setup
   const configs = new DocumentBuilder()
