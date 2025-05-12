4f481a883d4e2ccde68f4e155eaa10ee070b2469
Kailas Walldoddi <kailashwall@gmail.com>
2025-05-12 19:26:54 +0400

    Changed configurations settings

--- rag-gateway/src/rag/rag.module.ts
+++ rag-gateway/src/rag/rag.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RAGService } from './rag.service';
 import { RAGController } from './rag.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule], 
   controllers: [RAGController],
   providers: [RAGService]
 })
